e3a06011883ebf74d187b7fab902a406ed60d564
Ricardo Rosa <dbaricardor@gmail.com>
2021-08-01 03:11:52 -0300

    Add Script "permissionChange"

---
+++ permissionChange.ps1
@@ -0,0 +1,12 @@
+﻿$GRUPO = 'GL_FINANCEIRO'
+
+
+   #Setar o Grupo a ser Removido ou Adicionado
+
+$USUARIOS = Get-Content C:\SCRIPTS\PERMISSAO.txt   #Diretorio onde buscar os CPF´s
+
+foreach($USUARIO in $USUARIOS)
+{
+   ADD-ADGroupMember -Identity $GRUPO -Members $USUARIO -Confirm:$false    #Remove para remover a permissão - ADD para adicionar
+   Write-Host $USUARIO
+}
